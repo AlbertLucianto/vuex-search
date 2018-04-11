7b44fec2fe9fa91b4a3a184e9806eeab344f7e2e
Albert Lucianto <albertlucianto@gmail.com>
2018-04-11 19:13:20 +0800

    [package] Add build scripts for modules

---
+++ build/webpack.umd.conf.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    'vuex-search': './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist/umd'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'VuexSearch'
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        },
+        output: {
+          comments: true,
+          beautify: true
+        },
+        mangle: false
+      },
+      parallel: true,
+      sourceMap: true
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+}
