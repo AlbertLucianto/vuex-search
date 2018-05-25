9424125a158a6d09c8dd0df8b9bd846226e78a95
Albert Lucianto <albertlucianto@gmail.com>
2018-05-25 08:10:14 +0700

    [Webpack] Update dist configs

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/web/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/web'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
